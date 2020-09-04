ecbd57c12d6bb03ac0fa3b75bd6c7418d15f695f
SuGar <1906006182@qq.com>
2020-09-04 22:15:12 +0800

    sugar

---
+++ F_Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DataCollect
+{
+    partial class F_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Main));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加设备ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolSBtn_DeviceManege = new System.Windows.Forms.ToolStripButton();
+            this.dockPanel1 = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1220, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加设备ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 添加设备ToolStripMenuItem
+            // 
+            this.添加设备ToolStripMenuItem.Name = "添加设备ToolStripMenuItem";
+            this.添加设备ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.添加设备ToolStripMenuItem.Text = "添加设备";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(45, 45);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolSBtn_DeviceManege});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1220, 50);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolSBtn_DeviceManege
+            // 
+            this.toolSBtn_DeviceManege.AutoSize = false;
+            this.toolSBtn_DeviceManege.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolSBtn_DeviceManege.Image = ((System.Drawing.Image)(resources.GetObject("toolSBtn_DeviceManege.Image")));
+            this.toolSBtn_DeviceManege.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolSBtn_DeviceManege.Name = "toolSBtn_DeviceManege";
+            this.toolSBtn_DeviceManege.Size = new System.Drawing.Size(45, 45);
+            this.toolSBtn_DeviceManege.Text = "toolStripButton1";
+            this.toolSBtn_DeviceManege.Click += new System.EventHandler(this.toolSBtn_DeviceManege_Click);
+            // 
+            // dockPanel1
+            // 
+            this.dockPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel1.Location = new System.Drawing.Point(0, 75);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.Size = new System.Drawing.Size(1220, 509);
+            this.dockPanel1.TabIndex = 2;
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1220, 584);
+            this.Controls.Add(this.dockPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "F_Main";
+            this.Text = "F_Main";
+            this.Load += new System.EventHandler(this.F_Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加设备ToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolSBtn_DeviceManege;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel1;
+    }
+}
---
+++ F_Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataCollect
+{
+    public partial class F_Main : Form
+    {
+        public F_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void F_Main_Load(object sender, EventArgs e)
+        {
+            F_DeviceManage f_DeviceManage = F_DeviceManage.GetInstance();
+            f_DeviceManage.Show(dockPanel1);
+            f_DeviceManage.DockTo(dockPanel1, DockStyle.Left);//左边停靠
+        }
+
+        private void toolSBtn_DeviceManege_Click(object sender, EventArgs e)
+        {
+            F_DeviceManage f_DeviceManage = F_DeviceManage.GetInstance();
+            f_DeviceManage.Show(dockPanel1);
+            f_DeviceManage.DockTo(dockPanel1, DockStyle.Left);//左边停靠
+        }
+    }
+}
